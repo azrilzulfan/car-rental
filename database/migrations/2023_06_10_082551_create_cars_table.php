33e2cd9906d0145e875c39bf2320fad2c44490cc
azrilzulfan <103384095+azrilzulfan@users.noreply.github.com>
2023-06-11 08:39:59 +0700

    Update 2023_06_10_082551_create_cars_table.php

--- database/migrations/2023_06_10_082551_create_cars_table.php
+++ database/migrations/2023_06_10_082551_create_cars_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('image')->nullable();
             $table->string('name');
-            $table->decimal('price', 8, 2);
+            $table->integer('price');
             $table->timestamps();
         });
     }
